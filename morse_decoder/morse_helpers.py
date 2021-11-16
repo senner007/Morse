188a210d0620ec1c3c9d144e42c9bdfae4891508
nig <nielshtg@hotmail.com>
2021-11-16 07:35:37 +0100

    fix categorical training

--- morse_decoder/morse_helpers.py
+++ morse_decoder/morse_helpers.py
@@ -172,6 +172,20 @@ def add_noise_random(params):
     
     return add_noise
 
+def cut_and_center(params):
+    def cut_and_center(train_images, lbls, image_target_size):
+        cut_images = []
+        # train_images = train_images.copy()
+
+        for i in range(len(train_images)):
+            train_images[i][:, int(lbls[i] * 1400) + 3:] = 0
+            padding = int(150 / 2)  - int((int(lbls[i] *1400) + 3) / 2)
+            train_image_padded = np.pad(train_images[i], [(0,0),(padding,0), (0,0)], mode='constant')[:, :1400]
+            cut_images.append(train_image_padded)
+
+        return (cut_images, lbls)
+    return cut_and_center
+
 
 def create_all_sets(train, labels, TEST_SPLIT_SIZE, VALIDATION_SPLIT_SIZE, shuffle_before_test_split=True):
 
