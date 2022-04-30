78e5a02c502587a52e6f1932d03352257b36ac65
nig <nielshtg@hotmail.com>
2022-04-30 16:50:38 +0200

    wip

--- morse_decoder/Image_Generator_helpers.py
+++ morse_decoder/Image_Generator_helpers.py
@@ -4,6 +4,8 @@ import random
 import pandas as pd
 from pandas import read_csv
 from io import BufferedReader
+import time
+np.set_printoptions(suppress=True)
 
 global_path = "training_data/MorseTrainSet_23"
 
@@ -28,20 +30,15 @@ set_paths = [
     Set_Paths("/RAW23/Long16_23_100.bin", "/RAW23/L16Index_23_100.bin", "/Words_23_100.csv")
 ]
 
-class Buffers_Class:
-    data_buffer : BufferedReader
-    index_array :  np.ndarray
-    def __init__(self, data_buffer, index_array):
-        self.data_buffer = data_buffer
-        self.index_array = index_array
+idx_signal_length = "signal_length"
+idx_current_position = "current_position"
+idx_scale_factor = "scale_factor"
 
 class Random_Item:
-    data_buffer : BufferedReader
-    index_array :  np.ndarray
+    buffer_path : str
     csv_row : pd.DataFrame
-    def __init__(self, data_buffer, index_array, csv_row):
-        self.data_buffer = data_buffer
-        self.index_array = index_array
+    def __init__(self, buffer_path, csv_row):
+        self.buffer_path = buffer_path
         self.csv_row = csv_row
 
 class DataSets:
@@ -52,53 +49,47 @@ class DataSets:
         self.global_path = global_path
         self.masks = masks
         self.__cache_csv_files()
-        self.buffer_store : list[Buffers_Class]= []
-        self.__set_buffers()
 
     def __cache_csv_files(self):
         for set_path in self.set_paths_list:
             csv: pd.DataFrame = read_csv(self.global_path + set_path.csv_file)
+            idx_buffer: BufferedReader = open(self.global_path + set_path.long16_index, "rb")
+            idx_array = np.reshape(np.fromfile(idx_buffer, dtype=np.float64), (-1,3))
+            idx_buffer.close()
+            csv[idx_signal_length] = idx_array[:,0]
+            csv[idx_current_position] = idx_array[:,1]
+            csv[idx_scale_factor] = idx_array[:,2]
             self.csv_files.append(csv)
 
-    def __set_buffers(self):
-        for setx in self.set_paths_list:
-            dta_buffer: BufferedReader = open(self.global_path + setx.long16_bin, "rb")
-            idx_buffer: BufferedReader = open(self.global_path + setx.long16_index, "rb")
-            idx_array = np.fromfile(idx_buffer, dtype=np.float64)
-            idx_buffer.close()
-
-            self.buffer_store.append(Buffers_Class(dta_buffer, np.reshape(idx_array, (-1,3))))
-
-    def get_item_from_csv(self, set_choice, item_choice):
+    def get_item_from_csv(self, set_choice):
         csv: pd.DataFrame = self.csv_files[set_choice]
-        return csv.iloc[item_choice]
+        return csv.sample()
 
     def get_random(self):
         random_range = np.arange(start=0, stop=len(self.set_paths_list), step=1)
         set_choice: int  = random.choice(random_range)
-        random_buffers = self.buffer_store[set_choice]
-        item_choice = np.random.randint(0 , random_buffers.index_array.shape[0])
-        return Random_Item(random_buffers.data_buffer, random_buffers.index_array[item_choice], self.get_item_from_csv(set_choice, item_choice))
-
-    def close_files(self):
-        for buffers in self.buffer_store:
-            buffers.data_buffer.close()     
+        data_buffer_path = self.global_path + self.set_paths_list[set_choice].long16_bin
+        return Random_Item(data_buffer_path, self.get_item_from_csv(set_choice))
+    
 
 def get_item(random_set: Random_Item):
 
-    siglen, currentpos, scalefac = random_set.index_array
-    siglen = np.int32(siglen)
-    currentpos = np.int32(currentpos)
-    data_buffered =  random_set.data_buffer
+    csv_row = random_set.csv_row
+
+    siglen = np.int32(csv_row[idx_signal_length].values[0])
+    currentpos = np.int32(csv_row[idx_current_position].values[0])
+    data_buffered: BufferedReader = open(random_set.buffer_path, "rb")
     #### numpy_data = np.fromfile(data_buffered, dtype=np.int16)
     byte_step = 2
 
-    data_buffered.seek(currentpos * byte_step)
+    data_buffered.seek(int(currentpos * byte_step))
     ints = np.zeros((siglen,), int)
     for x in range(siglen):
         raw = data_buffered.read(byte_step)
         ints[x] = int.from_bytes(raw, byteorder="little", signed=True)
+
+    data_buffered.close()
     
     #### print(ints)
     #### print( numpy_data[currentpos:currentpos + siglen])
-    return np.float64(ints)*scalefac
+    return np.float64(ints) * csv_row[idx_scale_factor].values.astype(np.float64)[0]
